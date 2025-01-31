00e482e31bf3aad1b2bb6b71dad18d2327b6ef07
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2025-01-31 14:40:34 -0300

    changin bg-color

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,7 +24,7 @@ export default async function Home({
 
   return (
     <>
-      <section className="pink_container">
+      <section className="blue_container ">
         <h1 className="heading">
           Pitch Your Startup, <br /> Connect With Entrepreneurs
         </h1>
