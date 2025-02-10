a0f88a99818536c48dd2a4213726d25a4a00e1ac
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2025-02-10 17:28:42 -0300

    Making the user page
    have some issues yet, but I will resolve them !
    The issues is on the form...

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,9 +1,13 @@
+import { StartupCardSkaleton } from '@/app/component/StartupCard';
+import UserStartups from '@/app/component/UserStartups';
+
 import { auth } from '@/lib/auth';
 import { client } from '@/sanity/lib/client';
 import { AUTHOR_BY_ID_QUERY } from '@/sanity/lib/queries';
+import Image from 'next/image';
 
 import { notFound } from 'next/navigation';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
@@ -22,6 +26,27 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
               {user.name}
             </h3>
           </div>
+          <Image
+            src={user.image}
+            alt={user.name}
+            width={220}
+            height={220}
+            className="profile_image"
+          />
+          <p className="text-30-extrabold mt-7 text-center bg-[rgb(174,219,235)] rounded-full p-2">
+            @{user?.username}
+          </p>
+          <p className="mt-1 text-center text-black">{user?.bio}</p>
+        </div>
+        <div className="fle-1 flex flex-col gap-5 lg:-mt-5">
+          <p className="text-30-bold">
+            {session?.id != id ? 'Your' : 'All'} Startups
+          </p>
+          <ul className="card_grid-sm">
+            <Suspense fallback={<StartupCardSkaleton />}>
+              <UserStartups id={id} />
+            </Suspense>
+          </ul>
         </div>
       </section>
     </>
